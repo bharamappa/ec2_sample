15d837e35202473ae528a5a742e6aa8e3496cef4
bharamappa <69684890+bharamappa@users.noreply.github.com>
2020-08-15 16:43:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
 <title>Page Title</title>
 </head>
 <body>
+echo "<body style='background-color:blue'>";
 
-<h1>Welcome</h1>
-<p>To Ec2 Bharmu</p>
-  <p>To Ec2 Bharmu1</p>
+<h1>Welcome< to DevOps Training/h1>
+<p>by Arub Billur</p>
+  <p>For more explanation please go through the following link</p>
+  echo " <a href="https://enterprisersproject.com/article/2019/8/devops-terms-10-essential-concepts">Click here</a>";
 
 </body>
 </html>
