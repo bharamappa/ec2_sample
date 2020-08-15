5b837f18bd6a0bc95f3585cac7ebed7688698993
bharamappa <69684890+bharamappa@users.noreply.github.com>
2020-08-15 16:54:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,43 @@
 
 <h1>Welcome to DevOps Training</h1>
 <p>by Arub Billur</p>
+  <h5>What is Devops</h5>
+  Devops is a culture more than a technology!! Devops helps us to deliver software by collaborating both Developers and IT Operations.
+
+One can divide and conquer the Devops culture by mastering below given tools/topics. Devops can be categorized as following,
+
+Version Control
+SVN
+GIT
+Perforce
+Build Tools
+Ant
+Maven
+Gradle
+Continuous Integration / Continuous Deployment
+Jenkins
+Urban Code Deploy
+Rundeck
+Scripting
+Python
+Ruby
+Perl
+Javascript
+Cloud Technology
+Microsoft Azure
+Amazon Web Services
+Oracle Cloud
+Linux Administration
+Monitoring Tools
+Configuration Management
+Chef
+Puppet
+Ansible
+Containerization & Virtualization Concepts
+Docker
+VM Ware
+Virtual Box
+
   <p>For more explanation please go through the following link</p>
   <a href="https://enterprisersproject.com/article/2019/8/devops-terms-10-essential-concepts">Click here</a>
 
