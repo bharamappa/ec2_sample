d9a283ec76f51174fb098a5392ad02eaf7f1a357
bharamappa <69684890+bharamappa@users.noreply.github.com>
2020-08-15 17:00:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,41 +9,41 @@
 <h1>Welcome to DevOps Training</h1>
 <p>by Arub Billur</p>
   <h5>What is Devops</h5>
-  Devops is a culture more than a technology!! Devops helps us to deliver software by collaborating both Developers and IT Operations.
+ <p> Devops is a culture more than a technology!! Devops helps us to deliver software by collaborating both Developers and IT Operations.
 
-One can divide and conquer the Devops culture by mastering below given tools/topics. Devops can be categorized as following,
+One can divide and conquer the Devops culture by mastering below given tools/topics. Devops can be categorized as following</p>
 
-Version Control
-SVN
+  <h3>Version Control</h3>
+<p>SVN
 GIT
-Perforce
-Build Tools
-Ant
+  Perforce</p>
+  <h3>Build Tools</h3>
+<p>Ant
 Maven
-Gradle
-Continuous Integration / Continuous Deployment
+  Gradle</p>
+  <h3>Continuous Integration / Continuous Deployment</h3>
 Jenkins
 Urban Code Deploy
 Rundeck
-Scripting
-Python
+  <h3>Scripting</h3>
+<p>Python
 Ruby
 Perl
-Javascript
-Cloud Technology
-Microsoft Azure
+  Javascript</p>
+<h3>Cloud Technology</h3>
+<p>Microsoft Azure
 Amazon Web Services
 Oracle Cloud
-Linux Administration
-Monitoring Tools
+Linux Administration</>
+<h3>Monitoring Tools</h3>
 Configuration Management
 Chef
 Puppet
 Ansible
-Containerization & Virtualization Concepts
-Docker
+  <h3>Containerization & Virtualization Concepts</h3>
+<p>Docker
 VM Ware
-Virtual Box
+  Virtual Box</p>
 
   <p>For more explanation please go through the following link</p>
   <a href="https://enterprisersproject.com/article/2019/8/devops-terms-10-essential-concepts">Click here</a>
